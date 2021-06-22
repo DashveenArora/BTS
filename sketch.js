514ce821a98b6e2db249c41140d8d962c1a932ba
DashveenArora <86305694+DashveenArora@users.noreply.github.com>
2021-06-22 19:49:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+  var box = createInput();
+  box.position(5,60);
+  var button = createButton("BTS");
+  button.position(200,200);
+  button.mousePressed();
+}
+
+function draw() {
+  background(220); 
+}
